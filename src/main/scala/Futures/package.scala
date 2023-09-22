bd664216b2b8d08233ac15aca2e0258b2d330037
shamoel <shamoel.ahmad@gmail.com>
2023-09-22 11:57:47 +0530

    Blocking futues

---
+++ src/main/scala/Futures/package.scala
@@ -0,0 +1,10 @@
+package object Futures {
+
+  def time[B](block: => B): Unit = {
+    val t0 = System.nanoTime()
+    block // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0) + "ns")
+  }
+
+}

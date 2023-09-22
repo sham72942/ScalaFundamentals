ef0a5bf6e657c46e892df64548cc36773d8b1d3b
shamoel <shamoel.ahmad@gmail.com>
2023-09-22 12:21:16 +0530

    Future onComplete

--- src/main/scala/Futures/Futures.scala
+++ src/main/scala/Futures/Futures.scala
@@ -5,6 +5,7 @@ import java.lang.Thread.sleep
 import java.util.concurrent.{Executors, ThreadFactory}
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.util.{Failure, Random, Success}
 
 object Futures extends App {
 
@@ -12,23 +13,43 @@ object Futures extends App {
   implicit val ex: ExecutionContext = ExecutionContext.fromExecutor(
     new java.util.concurrent.ForkJoinPool(initialParallelism)
   )
-  nonBlockingFutures
+  println("timing nonBlockingFutures")
+  time(nonBlockingFutures())
+  println("timing blockingFutures")
+  time(blockingFutures())
+  println("timing usingOnComplete")
+  time(usingOnComplete())
 
   def two = Future {
     sleep(500)
     1 + 1
   }
 
-  def blockingFutures() = {
+  def blockingFutures(): Unit = {
     1 to 10 foreach { _ =>
-      val result = Await.result(two, Duration("1 second"))
-      println(result)
+      println(Await.result(two, Duration("1 second")))
     }
   }
 
-  def nonBlockingFutures() = {
+  def nonBlockingFutures(): Unit = {
     1 to 10 foreach { _ =>
       two.map(println)
+    }
+  }
+
+  def usingOnComplete(): Unit = {
+    println("starting calculation ...")
+    val f = Future {
+      val rand = Random.nextInt(500)
+      sleep(rand)
+      rand
+    }
+    println("before onComplete")
+    1 to 10 foreach { _ =>
+      f.onComplete {
+        case Success(value) => print(s"Got the callback, meaning = $value;\t")
+        case Failure(e)     => e.printStackTrace()
+      }
     }
   }
 
